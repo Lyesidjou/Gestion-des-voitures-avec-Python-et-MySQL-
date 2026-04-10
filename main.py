5b3858c63d67ff9dd6360ef81e43f1a8c372ddb5
Lyes Idjourdikane <lyes.idjourdikane@gmail.com>
2026-04-10 13:59:30 -0400

    9éme commit : creation de 4 objet voiture dans le main.py et j'ai appeler les 4 objet avec ajouter_voiture() pour les inserer dans la table voiture ensuite j'ai verifier ma base de donnee et les voiture on etait bien inserer

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from crud_db import connecter_db
+from crud_db import ajouter_voiture
+from voiture import Voiture
+
 con=connecter_db()
 print(con)
 con.close()
+
+v1=Voiture("HONDA","Civic",2018,34000)
+v2=Voiture("Toyota","Supra",2019,67000)
+v3=Voiture("Buggati","Chiron",2018,3000000)
+v4=Voiture("Lamborgini","Huracan",2012,500000)
+
+ajouter_voiture(v1)
+ajouter_voiture(v2)
+ajouter_voiture(v3)
+ajouter_voiture(v4)
+
